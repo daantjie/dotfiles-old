1691493be76dd8232386f24680901d577060b72c
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-02-11 17:20:05 +0000

    Add markdown-mode

--- emacs/init.el
+++ emacs/init.el
@@ -233,6 +233,16 @@ packages))
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
 
+;;; ==================== Markdown ====================
+
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
 ;;; =============== Tex ===============
 
 
