824d375cefd1420a849f614a5d1698e05b5ac01e
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-02-11 17:12:46 +0000

    Add multiple-cursors and bindings for it

--- emacs/init.el
+++ emacs/init.el
@@ -209,6 +209,17 @@ packages))
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
 (global-fci-mode 1)
 
+;;; ==================== Editing ====================
+
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
 
 ;;; ==================== Lisp ====================
 
